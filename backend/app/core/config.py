fa9d122a37df3704a1dc45f4171da315036c8396
Krishna <krishnakjee2016@gmail.com>
2026-07-06 02:50:08 +0530

    chore: secure backend with production cors middleware

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,12 @@
 import os
 from typing import List
 from dotenv import dotenv_values
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 config = dotenv_values(".env")
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "PulseLIMS API"
+    PROJECT_NAME: str = "PulseLIMS Advanced Cloud API"
     DESCRIPTION: str = "PulseLIMS API for managing laboratory information and patient data."
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
@@ -14,13 +14,17 @@ class Settings(BaseSettings):
     REDOC_URL: str = "/redoc"
     SECRET_KEY: str = config.get('SECRET_KEY')
     ALGORITHM: str = config.get('ALGORITHM')
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
-    BACKEND_CORS_ORIGINS: List[str] = [
-        "http://localhost:3000",
-    ]
+    ORIGINS: str = config.get('ORIGINS')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = config.get('ACCESS_TOKEN_EXPIRE_MINUTES')
 
-    class Config:
-        case_sensitive = True
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive = True, extra="ignore")
+
+    @property
+    def get_origins_list(self) -> List[str]:
+        return [origin.strip() for origin in self.ORIGINS.split(",")]
+
+    # class Config:
+    #     case_sensitive = True
+    #     env_file = ".env"
     
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.core.database import engine, Base
-from app.routers import auth, health
 
 
 try:
@@ -22,12 +21,15 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=settings.get_origins_list,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
 
-app.include_router(health.router)
-app.include_router(auth.router, prefix=f"{settings.API_V1_STR}", tags=["Authentication"])
+# app.include_router(auth.router, prefix=f"{settings.API_V1_STR}", tags=["Authentication"])
+
+@app.get("/")
+def read_root():
+    return {"message": "LIMS API Engine is running smoothly."}
